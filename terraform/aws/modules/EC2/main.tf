3dc5193b205319425e118f179e47d0f996fd244a
Adi-K527 <adikandakurtikar2@gmail.com>
2025-08-02 12:25:52 -0500

    containerized services

--- terraform/aws/modules/EC2/main.tf
+++ terraform/aws/modules/EC2/main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "instance" {
 
     curl -sfL https://get.k3s.io | sh -
 
-    sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+    sudo chmod 777 /etc/rancher/k3s/k3s.yaml
   EOF
   tags = {
     Name = var.name

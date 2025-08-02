162bd6f81ba4f55b7d2bdcb57864a7962312b13c
Adi-K527 <adikandakurtikar2@gmail.com>
2025-08-01 23:53:21 -0500

    server provisioning

--- terraform/modules/EC2/main.tf
+++ terraform/modules/EC2/main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "instance" {
   ami = "ami-0c02fb55956c7d316"
   instance_type = "t3.small"
   key_name = "terraform-key"
-  subnet_id = data.aws_subnet.default.ids[0]
+  subnet_id = data.aws_subnet.default.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   associate_public_ip_address = true

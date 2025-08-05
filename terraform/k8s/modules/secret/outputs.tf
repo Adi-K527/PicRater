0f36cae0814a05004ac72f6482b4dd95ed652d8b
Adi-K527 <adikandakurtikar2@gmail.com>
2025-08-05 16:03:31 -0500

    provision k3s

--- terraform/k8s/modules/secret/outputs.tf
+++ terraform/k8s/modules/secret/outputs.tf
@@ -1,4 +1,4 @@
 output "secret_name" {
   description = "The name of the Kubernetes secret created for ECR authentication"
-  value       = kubernetes_secret.example.metadata[0].name
+  value       = kubernetes_secret.ecr_secret.metadata[0].name
 }
